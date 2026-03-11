2cfac5363664088e049745887d1de7e0edd7c555
omeletteZ <arinabrachcovcka2005@gmail.com>
2026-03-11 23:49:25 +0200

    Reapply "omlet5"
    This reverts commit edf91d402529fa9666415adc9e7028c5cf46eb7e.

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,3 +1,4 @@
+import { cacheLife } from 'next/cache'
 import TaskList from "@/components/TaskList/TaskList";
 
 type Todo = {
@@ -7,10 +8,15 @@ type Todo = {
   completed: boolean;
 };
 
-export default async function Page() {
+async function getTasks(): Promise<Todo[]> {
+  'use cache'
+  cacheLife('hours')
   const res = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10");
-  const todos: Todo[] = await res.json();
+  return res.json();
+}
 
+export default async function Page() {
+  const todos = await getTasks();
   return (
     <div>
       <TaskList todos={todos} />
